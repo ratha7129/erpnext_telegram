b2986bfbb015f4934dc5942c276e90af3c251d4a
ratha7129 <53745976+ratha7129@users.noreply.github.com>
2023-07-08 17:24:04 +0700

    update 2023-07-08 5:23 PM

--- erpnext_telegram_integration/erpnext_telegram_integration/doctype/telegram_settings/telegram_settings.py
+++ erpnext_telegram_integration/erpnext_telegram_integration/doctype/telegram_settings/telegram_settings.py
@@ -18,8 +18,9 @@ class TelegramSettings(Document):
 
 @frappe.whitelist()
 def send_to_telegram(telegram_user, message, reference_doctype=None, reference_name=None, attachment=None):
-    frappe.enqueue(send_to_telegram_queue(telegram_user, message, reference_doctype, reference_name, attachment),queue="long")
-    return "Added To Queue"
+    frappe.enqueue(
+    send_to_telegram_queue(telegram_user, message, reference_doctype, reference_name, attachment),queue="short",timeout=4000)
+    return "Add To Queue"
 
 @frappe.whitelist()
 def send_to_telegram_queue(telegram_user, message, reference_doctype=None, reference_name=None, attachment=None):
